8a380b221a37b378a804824b0f655acc82150bed
ahmadmm01 <skyahmad74@gmail.com>
2022-04-08 02:33:25 +0700

    Update Package Database

--- app/src/main/java/com/pam/activity6/database/DBController.java
+++ app/src/main/java/com/pam/activity6/database/DBController.java
@@ -40,4 +40,27 @@ public class DBController extends SQLiteOpenHelper
         basisdata.insert("teman", null, nilai);
         basisdata.close();
     }
+
+    public ArrayList<HashMap<String, String>> getAllTeman()
+    {
+        ArrayList<HashMap<String, String>> daftarTeman;
+        daftarTeman = new ArrayList<HashMap<String, String>>();
+        String selectQuery = "Select * from teman";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if(cursor.moveToFirst())
+        {
+            do
+            {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("id", cursor.getString(0));
+                map.put("nama", cursor.getString(1));
+                map.put("telpon", cursor.getString(2));
+                daftarTeman.add(map);
+            }
+            while (cursor.moveToNext());
+        }
+        db.close();
+        return daftarTeman;
+    }
 }
