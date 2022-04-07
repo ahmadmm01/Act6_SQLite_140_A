d76620d36c82bb4d9eb9082eb61f712f09012f44
ahmadmm01 <skyahmad74@gmail.com>
2022-04-08 03:06:51 +0700

    Update Activity MainActivity

--- app/src/main/java/com/pam/activity6/MainActivity.java
+++ app/src/main/java/com/pam/activity6/MainActivity.java
@@ -1,14 +1,68 @@
 package com.pam.activity6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pam.activity6.adapter.TemanAdapter;
+import com.pam.activity6.database.DBController;
+import com.pam.activity6.database.Teman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity
+{
+    private RecyclerView recyclerView;
+    private TemanAdapter adapter;
+    private ArrayList<Teman> temanArrayList;
+    DBController controller = new DBController(this);
+    String id, nm, tlp;
+    private FloatingActionButton fab;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleView);
+        fab = findViewById(R.id.floatingBtn);
+        BacaData();
+        adapter = new TemanAdapter(temanArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this,TemanBaru.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void BacaData()
+    {
+        ArrayList<HashMap<String, String>> daftarTeman = controller.getAllTeman();
+        temanArrayList = new ArrayList<>();
+
+        for (int i=0; i<daftarTeman.size(); i++)
+        {
+            Teman teman = new Teman();
+
+            teman.setId(daftarTeman.get(i).get("id").toString());
+            teman.setNama(daftarTeman.get(i).get("nama").toString());
+            teman.setTelpon(daftarTeman.get(i).get("telpon").toString());
+
+            temanArrayList.add(teman);
+        }
     }
 }
---
+++ app/src/main/java/com/pam/activity6/TemanBaru.java
@@ -0,0 +1,14 @@
+package com.pam.activity6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TemanBaru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman_baru);
+    }
+}
